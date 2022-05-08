b4fd50cae976f0dc1bbf32b9b46be511fae239e7
Soner Can Zencirkıran <44876635+SonerCzk@users.noreply.github.com>
2022-05-08 14:02:19 +0300

    Update Multi-Layer Perceptron Neural Network.py

--- Sklearn/Multi-Layer Perceptron Neural Network.py
+++ Sklearn/Multi-Layer Perceptron Neural Network.py
@@ -4,22 +4,24 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_diabetes
 from sklearn.neural_network import MLPRegressor
 
-#X, y = load_breast_cancer(return_X_y=True)
-#print(X)
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
-#                                                    random_state=2)
+X, y = load_breast_cancer(return_X_y=True)
 
-#clf = MLPClassifier(random_state=1, max_iter=300).fit(X_train, y_train)
+print(X)
 
-#clf.score(X_test, y_test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
+                                                    random_state=2)
+
+clf = MLPClassifier(random_state=1, max_iter=300).fit(X_train, y_train)
+
+clf.score(X_test, y_test)
 
 
 
 A, b = load_diabetes(return_X_y=True)
 A_train, A_test, b_train, b_test = train_test_split(A, b,test_size=0.2,
                                                     random_state=2)
-regr = MLPRegressor(random_state=1, max_iter=2000).fit(A_train, b_train)
+regr = MLPRegressor(random_state=1, max_iter=500).fit(A_train, b_train)
 
-print(len(A))
+print(A)
 
 regr.score(A_test, b_test)
